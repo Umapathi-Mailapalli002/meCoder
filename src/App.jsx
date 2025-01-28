a5812a58223b1fcea0de439963af296609b5c35f
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-01-28 08:42:37 +0530

    initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-red-600 text-white'>this is new H1 tag</h1>
+    </>
+  )
+}
+
+export default App
