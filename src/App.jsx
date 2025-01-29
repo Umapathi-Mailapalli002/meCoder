b03120fe8e4d1193da552632d6e7335bef33d666
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2025-01-29 07:46:29 +0530

    refactor: remove Tailwind CSS, integrate Chakra UI, and implement in-browser code editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
+import { Box } from "@chakra-ui/react";
+import CodeEditor from "./components/CodeEditor";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className='bg-red-600 text-white'>this is new H1 tag</h1>
-    </>
-  )
+    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+      <CodeEditor />
+    </Box>
+  );
 }
 
-export default App
+export default App;
